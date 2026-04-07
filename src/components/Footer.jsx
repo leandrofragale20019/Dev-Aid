e841ed54b3ea8615c3f1022f1b2ea04bd551e95b
leandrofragale20019 <leandrofragale827@gmail.com>
2026-04-07 14:04:42 +0200

    initial project setup

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,17 @@
 import "../styles/Footer.css";
 
 function Footer() {
-const currentYear = new Date().getFullYear();
+  const currentYear = new Date().getFullYear();
 
   return (
     <footer>
       <div>© Dev-Aid {currentYear} — open source</div>
       <div className="footer-links">
         <a href="https://github.com/Dev-Aid-Team/Dev-Aid">GitHub</a>
-        <a href="https://github.com/Dev-Aid-Team/Dev-Aid/discussions/1">Feedback</a>
-        <a href="#v1">V1.0.0</a>
+        <a href="https://github.com/Dev-Aid-Team/Dev-Aid/discussions/1">
+          Feedback
+        </a>
+        <a href="#v1">V1.0.1</a>
       </div>
     </footer>
   );
