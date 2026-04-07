174a836c86603f53c9dfc628647644b4f943dbcf
segg3 <damian.segginger@coop.ch>
2026-04-07 09:12:32 +0200

    Updated Footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,8 +7,8 @@ const currentYear = new Date().getFullYear();
     <footer>
       <div>© Dev-Aid {currentYear} — open source</div>
       <div className="footer-links">
-        <a href="https://github.com/leandrofragale20019/Dev-Aid">GitHub</a>
-        <a href="https://github.com/leandrofragale20019/Dev-Aid/discussions">Feedback</a>
+        <a href="https://github.com/Dev-Aid-Team/Dev-Aid">GitHub</a>
+        <a href="https://github.com/Dev-Aid-Team/Dev-Aid/discussions/1">Feedback</a>
         <a href="#v1">V1.0.0</a>
       </div>
     </footer>
