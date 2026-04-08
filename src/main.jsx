db16849522dacb926b9044c4acb16201f9d7d12b
DamiSeggi <damiansegginger@gmail.com>
2026-04-08 12:59:45 +0200

    Remove duplicate css definitions in tools

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
 import './styles/global.css';
+import './styles/tools/Tools.css'
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
